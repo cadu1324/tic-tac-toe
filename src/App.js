3adf48000b9a290ba393eb9f64cd13082700a1ce
Carlos <caduzin11324@gmail.com>
2022-03-15 20:49:34 -0300

    making the game body

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Square from "./components/Squares";
+import { useState } from "react";
 
 function App() {
+
+  const style = {
+    border: "4px solid darkblue",
+    borderRadius: "10px",
+    width: "500px",
+    height: "500px",
+    margin: "200px auto",
+    display: "grid",
+    gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+  };
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    // for (let i = 0; i < lines.length; i++) {
+    //   const [a, b, c] = lines[i];
+    //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    //     return squares[a];
+    //   }
+    // }
+    return null;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={style}>
+        <Square value="1" onClick={() =>{console.log('alo')}} />
+        <Square value="2" onClick={() =>{console.log('alo')}} />
+        <Square value="3" onClick={() =>{console.log('alo')}} />
+        <Square value="4" onClick={() =>{console.log('alo')}} />
+        <Square value="5" onClick={() =>{console.log('alo')}} />
+        <Square value="6" onClick={() =>{console.log('alo')}} />
+        <Square value="7" onClick={() =>{console.log('alo')}} />
+        <Square value="8" onClick={() =>{console.log('alo')}} />
+        <Square value="9" onClick={() =>{console.log('alo')}} />
+      </div>
     </div>
   );
 }
